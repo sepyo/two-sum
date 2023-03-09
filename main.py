27419ed53dcc4a989ffc3cd8e78e401736571562
Yosep Kim <yskim0830@outlook.com>
2023-03-09 20:58:56 +0900

    feat: 메소드가 아닌 함수는 main에 있습니다.

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 from itertools import combinations
-#from typing import List
-
-def twoSum(nums, target):
-#def twoSum(self, nums:List[int], target:int)->List[int]:
+from typing import List
+#def twoSum(nums, target):
+#class CCLASS:
+def twoSum(self, nums:List[int], target:int)->List[int]:
+#def twoSum(nums:List[int], target:int)->List[int]:
     for i, j in combinations(range(len(nums)), 2):
         if nums[i] + nums[j] == target:
             return [i, j]
     return [-1, -1]
-
+#b = CCLASS()
 #a = [1, 3, 4, 2, 8, 9, 1, 3013 ,3,1, 3,1,3,  1, 3]
+
+#print(b.twoSum(a, 3017))
 #print(twoSum(a, 3017))
 
